1d47afbf77d715618df19e0ca12d1219ea10c443
HyagoGabriel <hyagogabriel3@gmail.com>
2024-10-16 12:58:55 -0300

    16/10

--- sistemavio/api/src/routes/apiRoutes.js
+++ sistemavio/api/src/routes/apiRoutes.js
@@ -5,12 +5,12 @@ const orgController = require("../controllers/orgController");
 
 router.post("/user", userController.createUser);
 router.get("/user", userController.getAllUsers);
-// router.put("/user", userController.updateUser);
-// router.delete("/user/:cpf", userController.deleteUser);
+router.put("/user", userController.updateUser);
+router.delete("/user/:id_usuario", userController.deleteUser);
 
 router.post("/org", orgController.createOrg);
 router.get("/org", orgController.getAllOrgs);
-router.put("/org/:id_organizador", orgController.updateOrg);
+router.put("/org", orgController.updateOrg);
 router.delete("/org/:id_organizador", orgController.deleteOrg);
 
 module.exports = router;
