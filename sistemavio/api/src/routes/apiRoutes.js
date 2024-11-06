b78a2c319bccba568f03af34e3e71df771e7ce1b
HyagoGabriel <hyagogabriel3@gmail.com>
2024-11-06 12:43:44 -0300

    atualizaçao_update

--- sistemavio/api/src/routes/apiRoutes.js
+++ sistemavio/api/src/routes/apiRoutes.js
@@ -3,6 +3,8 @@ const router = require("express").Router();
 const userController = require("../controllers/userController");
 const orgController = require("../controllers/orgController");
 
+const eventoController = require("../controllers/eventoController");
+
 router.post("/user", userController.createUser);
 router.get("/user", userController.getAllUsers);
 router.put("/user", userController.updateUser);
@@ -12,5 +14,11 @@ router.post("/org", orgController.createOrg);
 router.get("/org", orgController.getAllOrgs);
 router.put("/org", orgController.updateOrg);
 router.delete("/org/:id_organizador", orgController.deleteOrg);
+
+//rotas eventoController
+router.post('/evento', eventoController.createEvento);
+router.get('/evento', eventoController.getAllEventos);
+router.put('/evento', eventoController.updateEvento);
+
 
 module.exports = router;
